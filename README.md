ed18c6a6fadb26c5bf83224cb01701a2101715a8
Patricio Belardo <pbelardo@Patricios-Mini.attlocal.net>
2019-06-20 09:28:23 -0400

    work

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # B2C Proof of Concept
 B2C PoC using additional steps to get group memberships at the IdP
 
-## Description
+Here is what we are doing here:
 
-### Custom Policies
+![](img/2019-06-20-09-25-39.png)
 
-Here is where we extended the user journey with a new technical profile.
+**Note**: This is not 100% accurate, but is to demostrate the additional step we are doing here
+
+## Custom Policies
+
+First, you should follow [this](https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-get-started-custom) process to use custom policies.
+
+In these files you will see:
+- pbcap.onmicrosoft.com: B2CTenant
+- cxtenant1.onmicrosoft.com: An IdP (AAD) representing a customer tenant
 
 > This approach is useful in case you are not receiving information about group memberships at the claim, or you are receiving just Ids. In case you want to enable B2C to include group membership at claims, with no additional steps, please refer to **Custom Policies (include group membership at claim)**
 
 Basically the changes are:
 
-#### TrustFrameworkBase.xml
+### TrustFrameworkBase.xml
 
 Added groupNames ClaimType
 ````xml
@@ -42,7 +50,7 @@ Added GetUserGroups TechnicalProfile
     </TechnicalProfile>
 ````
 
-#### TrustFrameworkExtensions.xml
+### TrustFrameworkExtensions.xml
 
 At one UserJourney, added an additional OrchestrationStep
 ````xml
@@ -53,22 +61,22 @@ At one UserJourney, added an additional OrchestrationStep
         </OrchestrationStep>
 ````
 
-#### SignUpOrSigninCxDomain1.xml
+### SignUpOrSigninCxDomain1.xml
 
 Added the groupNames outputclaim
 ````xml
 <OutputClaim ClaimTypeReferenceId="groupNames" />
 ````
 
-### Custom Policies (include group membership at claim)
+## Custom Policies (include group membership at claim)
 
 This is an **optional** step. 
 
+> If you are using AAD, you will need to change the app registration manifest to include group membership information, as explained [here](https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-fed-group-claims#configure-the-azure-ad-application-registration-for-group-attributes)
+
 In case you want to add the group membership you are receiving from your IdP as an output claim at B2C, you can do it changing the following files:
 
-> In case of AAD, will need to change the app registration manifest to include group membership information, as explained [here](https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-fed-group-claims#configure-the-azure-ad-application-registration-for-group-attributes)
-
-#### TrustFrameworkBase.xml
+### TrustFrameworkBase.xml
 
 Added 2 ClaimTypes
 ````xml
@@ -83,14 +91,14 @@ Added 2 ClaimTypes
 </ClaimType>
 ````
 
-#### TrustFrameworkExtension.xml
+### TrustFrameworkExtension.xml
 
 At the TechnicalProfile located at the ClaimsProvider that points to your IdP, addded the following OutputClaims
 ````xml
 <OutputClaim ClaimTypeReferenceId="AADGroups" PartnerClaimType="groups"/>
 ````
 
-#### SignUpOrSigninCxDomain1.xml
+### SignUpOrSigninCxDomain1.xml
 
 Added the AADGroups outputclaim
 ````xml
@@ -99,8 +107,86 @@ Added the AADGroups outputclaim
 
 Now, your application that interacts with B2C will receive an AADGroups claim with groups information thet we god from the IdP.
 
-### Membership information API
+## Membership information API
 
 This is an Web API that uses Microsoft.Graph and will get group names based on a user membership (objectId).
 
+> It is important to use the same parameter name as the name of the input parameter at the TechnicalProfile, in my case "issuerUserId"
 
+Here is the principal method:
+
+````c#
+        [HttpGet("GroupNames")]
+        public async Task<ActionResult<JObject>> GetMembership(string issuerUserId)
+        {
+            if (string.IsNullOrEmpty(issuerUserId))
+            {
+                return new JObject { "You must include a userid" };
+            }
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+            IConfidentialClientApplication app;
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                            .WithClientSecret(config.ClientSecret)
+                            .WithAuthority(new Uri(config.Authority))
+                            .Build();
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+            JObject returnValue = new JObject();
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                returnValue = await apiCaller.CallWebApiASync($"https://graph.microsoft.com/v1.0/users/{issuerUserId}/memberOf", result.AccessToken);
+            }
+
+            var groupArray = (JArray)returnValue["value"];
+            var groupNames = new List<string>();
+            if (groupArray != null)
+            {
+                foreach (JObject g in groupArray)
+                {
+                    var name = g["displayName"].Value<string>();
+                    groupNames.Add(name);
+                }
+            }
+            return new JsonResult(
+               new
+               {
+                   groupNames
+               });
+        }
+````
+
+## Results
+
+Here are the claims I received after changes:
+
+![](img/2019-06-20-09-05-03.png)
+
+
+### Notes
+You can use the [test](https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-get-started-custom#test-the-custom-policy) application at B2C. An additional resource with information about how to test it is located [here](https://saraford.net/2017/09/18/how-to-review-your-azure-ad-b2c-tokens-using-policy-run-now-and-jwt-ms/).
+
+Another option is to use [this](https://github.com/Azure-Samples/active-directory-b2c-dotnetcore-webapp) application to see the claims output at the about page.
+
+**Remember**. You can add [AppInsights](https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-troubleshoot-custom) traces for debugging!
---
+++ src/ADHelperAPI/ADHelperAPI/Controllers/MembershipController.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Threading;
+using Microsoft.Identity.Client;
+
+namespace ADHelperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembershipController : ControllerBase
+    {
+        [HttpGet("GroupNames")]
+        public async Task<ActionResult<JObject>> GetMembership(string issuerUserId)
+        {
+            if (string.IsNullOrEmpty(issuerUserId))
+            {
+                return new JObject { "You must include a userid" };
+            }
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+            IConfidentialClientApplication app;
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                            .WithClientSecret(config.ClientSecret)
+                            .WithAuthority(new Uri(config.Authority))
+                            .Build();
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+            JObject returnValue = new JObject();
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                returnValue = await apiCaller.CallWebApiASync($"https://graph.microsoft.com/v1.0/users/{issuerUserId}/memberOf", result.AccessToken);
+            }
+
+            var groupArray = (JArray)returnValue["value"];
+            var groupNames = new List<string>();
+            if (groupArray != null)
+            {
+                foreach (JObject g in groupArray)
+                {
+                    var name = g["displayName"].Value<string>();
+                    groupNames.Add(name);
+                }
+            }
+            return new JsonResult(
+               new
+               {
+                   groupNames
+               });
+        }
+
+        // GET api/values
+        [HttpGet("{groupid}")]
+        public async Task<ActionResult<JObject>> Get(string groupid)
+        {
+            if (string.IsNullOrEmpty(groupid))
+            {
+                return new JObject { "You must include a group id" };
+            }
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+            IConfidentialClientApplication app;
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                            .WithClientSecret(config.ClientSecret)
+                            .WithAuthority(new Uri(config.Authority))
+                            .Build();
+// With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+            JObject returnValue = new JObject();
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                // await apiCaller.CallWebApiAndProcessResultASync($"https://graph.microsoft.com/v1.0/groups/{groupid}", result.AccessToken, Display);
+                returnValue = await apiCaller.CallWebApiASync($"https://graph.microsoft.com/v1.0/groups/{groupid}", result.AccessToken);
+            }
+
+
+
+            return returnValue;
+        }
+        private static void Display(JObject result)
+        {
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
+        }
+        // public static string JsonPrettify(string json)
+        // {
+        //     using (var stringReader = new StringReader(json))
+        //     using (var stringWriter = new StringWriter())
+        //     {
+        //         var jsonReader = new JsonTextReader(stringReader);
+        //         var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+        //         jsonWriter.WriteToken(jsonReader);
+        //         return stringWriter.ToString();
+        //     }
+        // }
+
+        // static async Task getMembers()
+        // {
+        //     httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+        //     Console.WriteLine("\n \n Retrieving users {0}", DateTime.Now.ToString());
+        //     HttpResponseMessage response = await httpClient.GetAsync(resourceUri + "/v1.0/users?$top=5");
+
+        //     if (response.IsSuccessStatusCode)
+        //     {
+        //         // Read the response and output it to the console.
+        //         string users = await response.Content.ReadAsStringAsync();
+        //         Console.WriteLine("\n \n Printing out Users \n \n");
+        //         Console.WriteLine(JsonPrettify(users));
+        //         Console.WriteLine("Received Info");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Failed to retrieve To Do list\nError:  {0}\n", response.ReasonPhrase);
+        //     }
+        // }
+
+        // static async Task prettyJWTPrint(String myToken)
+        // {
+        //     //Assume the input is in a control called txtJwtIn,
+        //     //and the output will be placed in a control called txtJwtOut
+        //     var jwtHandler = new JwtSecurityTokenHandler();
+        //     var jwtInput = myToken;
+        //     String prettyPrint = "";
+
+        //     //Check if readable token (string is in a JWT format)
+        //     var readableToken = jwtHandler.CanReadToken(jwtInput);
+
+        //     if (readableToken != true)
+        //     {
+        //         Console.WriteLine("The token doesn't seem to be in a proper JWT format.");
+        //     }
+        //     if (readableToken == true)
+        //     {
+        //         var token = jwtHandler.ReadJwtToken(jwtInput);
+
+        //         //Extract the headers of the JWT
+        //         var headers = token.Header;
+        //         var jwtHeader = "{";
+        //         foreach (var h in headers)
+        //         {
+        //             jwtHeader += '"' + h.Key + "\":\"" + h.Value + "\",";
+        //         }
+        //         jwtHeader += "}";
+        //         prettyPrint = "Header:\r\n" + JToken.Parse(jwtHeader).ToString(Formatting.Indented);
+
+        //         //Extract the payload of the JWT
+        //         var claims = token.Claims;
+        //         var jwtPayload = "{";
+        //         foreach (System.Security.Claims.Claim c in claims)
+        //         {
+        //             jwtPayload += '"' + c.Type + "\":\"" + c.Value + "\",";
+        //         }
+        //         jwtPayload += "}";
+        //         prettyPrint += "\r\nPayload:\r\n" + JToken.Parse(jwtPayload).ToString(Formatting.Indented);
+
+        //     }
+        //     Console.WriteLine(prettyPrint);
+        // }
+        // static async Task getAccessToken()
+        // {
+        //     int retryCount = 0;
+        //     bool retry = false;
+        //     authContext = new AuthenticationContext(authority + tenantID);
+
+        //     do
+        //     {
+        //         retry = false;
+        //         try
+        //         {
+        //             // ADAL includes an in memory cache, so this call will only send a message to the server if the cached token is expired.
+        //             result = await authContext.AcquireTokenAsync(resourceUri, clientId, new Uri(redirectUri), new PlatformParameters(PromptBehavior.Never, new CustomWebUi()));
+
+        //             Console.Write("My Access Token : \n");
+        //             await prettyJWTPrint(result.AccessToken);
+        //         }
+        //         catch (AdalException ex)
+        //         {
+        //             if (ex.ErrorCode == "temporarily_unavailable")
+        //             {
+        //                 retry = true;
+        //                 retryCount++;
+        //                 Thread.Sleep(3000);
+        //             }
+
+        //             Console.WriteLine(
+        //                 String.Format("An error occurred while acquiring a token\nTime: {0}\nError: {1}\nRetry: {2}\n",
+        //                 DateTime.Now.ToString(),
+        //                 ex.ToString(),
+        //                 retry.ToString()));
+        //         }
+
+        //     } while ((retry == true) && (retryCount < 3));
+
+        //     if (result == null)
+        //     {
+        //         Console.WriteLine("Canceling attempt to get access token.\n");
+        //         return;
+        //     }
+
+        // }
+
+    }
+}
